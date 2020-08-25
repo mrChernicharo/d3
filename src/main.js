9b961535a46e3c32e7d2c82699189a0cbaceaf16
Felipe Chernicharo <string7dev@gmail.com>
2020-08-25 13:27:54 -0300

    svg shapes

--- src/main.js
+++ src/main.js
@@ -86,21 +86,39 @@ import { attrs } from "d3-selection-multi";
 // ================================ aula ================================----
 // ================================ [Selection-Multi] ================================---
 
-// ❯ yarn add d3-selection-multi
+// // ❯ yarn add d3-selection-multi
+
+// let dim = {
+//   width: 400,
+//   height: 300,
+// };
+
+// let circleAttrs = {
+//   cx: (d) => d,
+//   cy: (d) => d,
+//   r: (d) => (Math.random() * d) / 8,
+//   fill: "red",
+// };
+
+// let d = [50, 100, 150, 200, 250];
+
+// let svg = d3
+//   .select("body")
+//   .append("svg")
+//   .style("background", "lightblue")
+//   .attrs(dim);
+
+// svg.selectAll("circle").data(d).enter().append("circle").attrs(circleAttrs);
+
+// ================================ aula ================================----
+// ================================ [SVG - Shapes] ================================---
+
+// let d = [50, 100, 150, 200, 250];
 
 let dim = {
-  width: 400,
-  height: 300,
+  width: 600,
+  height: 400,
 };
-
-let circleAttrs = {
-  cx: (d) => d,
-  cy: (d) => d,
-  r: (d) => (Math.random() * d) / 8,
-  fill: "red",
-};
-
-let d = [50, 100, 150, 200, 250];
 
 let svg = d3
   .select("body")
@@ -108,10 +126,52 @@ let svg = d3
   .style("background", "lightblue")
   .attrs(dim);
 
-svg.selectAll("circle").data(d).enter().append("circle").attrs(circleAttrs);
+let attributes = {
+  cx: 100,
+  cy: 100,
+  r: 40,
+};
+svg.append("circle").attrs(attributes);
 
-// ================================ aula ================================----
-// ================================ [] ================================---
+attributes = {
+  cx: 300,
+  cy: 100,
+  rx: 60,
+  ry: 30,
+};
+svg.append("ellipse").attrs(attributes);
+
+attributes = {
+  x1: 450,
+  y1: 150,
+  x2: 550,
+  y2: 50,
+  stroke: "black",
+};
+svg.append("line").attrs(attributes);
+
+attributes = {
+  points: "50,250 100,350 150,300 150,350", // x,y x,y x,y x,y
+  stroke: "black",
+  fill: "none",
+};
+svg.append("polygon").attrs(attributes);
+
+attributes = {
+  points: "250,250 300,350 350,300 350,350", // x,y x,y x,y x,y
+  stroke: "black",
+  fill: "none",
+};
+svg.append("polyline").attrs(attributes);
+
+attributes = {
+  x: 450,
+  y: 300,
+  width: 100,
+  height: 80,
+  rx: 10, // border-radius
+};
+svg.append("rect").attrs(attributes);
 
 // ================================ aula ================================----
 // ================================ [] ================================---
