e580fe0aec285bd32e7d3ddc1b65028ef38afe2b
Felipe Chernicharo <string7dev@gmail.com>
2020-08-26 12:55:35 -0300

    grid

--- src/04.axis.js
+++ src/04.axis.js
@@ -27,6 +27,40 @@ import { attrs } from 'd3-selection-multi';
 // ================================ aula 29 ================================----
 // ================================ [Axis Styling] ================================---
 
+// const dim = {
+//   width: 600,
+//   height: 400,
+// };
+
+// const svgCanvas = d3.select('body').append('svg').style('background', 'lightgray').attrs(dim);
+
+// let data = [10, 29, 130, 240, 250, 360, 370, 610, 820, 480, 690, 510];
+// let months = ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'];
+
+// const scaleX = d3.scalePoint(months, [50, 550]); // ([domain], [range])
+
+// const scaleY = d3.scaleLinear(d3.extent(data), [350, 50]); // ([domain], [range])
+
+// let axisX = d3.axisBottom(scaleX);
+// let axisY = d3.axisLeft(scaleY);
+
+// // axisY.ticks(10);
+// axisY.tickValues([40, 100, 300, 500, d3.max(data)]);
+// // axisY.tickFormat((d) => '$ ' + d);
+// axisX.tickSize(15);
+
+// svgCanvas
+//   .append('g')
+//   .attr('transform', 'translate(0, 350)')
+//   .call(axisX)
+//   .selectAll('text')
+//   .attrs({ 'font-size': 12, transform: 'translate(-24, 10), rotate(320)' });
+
+// svgCanvas.append('g').attr('transform', 'translate(50, 0)').call(axisY);
+
+// ================================ aula 30 ================================----
+// ================================ [Grid] ================================---
+
 const dim = {
   width: 600,
   height: 400,
@@ -43,26 +77,29 @@ const scaleY = d3.scaleLinear(d3.extent(data), [350, 50]); // ([domain], [range]
 
 let axisX = d3.axisBottom(scaleX);
 let axisY = d3.axisLeft(scaleY);
+let gridY = d3.axisLeft(scaleY);
 
-// axisY.ticks(10);
-axisY.tickValues([40, 100, 300, 500, d3.max(data)]);
+axisY.ticks(10);
+// axisY.tickValues([40, 100, 300, 500, d3.max(data)]);
 // axisY.tickFormat((d) => '$ ' + d);
-axisX.tickSize(15);
+gridY.tickSize(-500).tickFormat('').tickSizeOuter(0);
 
 svgCanvas
   .append('g')
-  .attr('transform', 'translate(0, 350)')
+  .attr('transform', 'translate(0, 360)')
   .call(axisX)
   .selectAll('text')
   .attrs({ 'font-size': 12, transform: 'translate(-24, 10), rotate(320)' });
 
 svgCanvas.append('g').attr('transform', 'translate(50, 0)').call(axisY);
 
-// ================================ aula 30 ================================----
-// ================================ [] ================================---
+svgCanvas.append('g').attr('transform', 'translate(50, 0)').call(gridY).selectAll('line').attrs({
+  stroke: 'grey',
+  'stroke-dasharray': '5, 3',
+});
 
 // ================================ aula 31 ================================----
-// ================================ [] ================================---
+// ================================ [Animations] ================================---
 
 // ================================ aula 32 ================================----
 // ================================ [] ================================---
